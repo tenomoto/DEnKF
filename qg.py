fix sign
FossilOrigin-Name: ad2615efdd0f5bed0982ba5ddf3306b85b4e312d708fdb80984d1e2698c50146

qg.py
@@ -5,32 +5,36 @@ from mg import v_cycle
 from ode import rk4
 
 
-def step(q, psi, y, f=1600, eps=1.0e-5, a=2.0e-12, itermax=(1, 1, 5000), tol=1.0e-4):
+def step(q, psi, y, c=1.0, f=1600, eps=1.0e-5, a=2.0e-12, tau0=tau, itermax=(1, 1, 5000), tol=1.0e-4):
     d = y[1] - y[0]
     d2 = d ** 2
-    psi, _ = v_cycle(psi, q, d, f, itermax, tol)
+    psi[:,:], _ = v_cycle(psi, q, d, f, itermax, tol)
     psix = np.zeros_like(psi)
+    dqdt = np.zeros_like(psi)
     psix[1:-1, 1:-1] = (psi[2:, 1:-1] - psi[:-2, 1:-1]) / d
     lap3psi = laplacian(laplacian(q + f * psi) / d2) / d2 
     jac = jacobian(psi, q) / d2
-    dqdt = -psix - eps * jac - a * lap3psi + tau * np.sin(tau * y[None,])
+    dqdt[1:-1, 1:-1] = (-c * psix - eps * jac - a * lap3psi + tau0 * np.sin(tau * y[None,]))[1:-1, 1:-1]
     return dqdt
 
 if __name__ == "__main__":
     n = 129 
     dt = 1.5
-    nstep = 100
-    nsave = 10
+    nstep = 1000
+    nsave = 100
     x = np.linspace(0.0, 1.0, n)
     y = np.linspace(0.0, 1.0, n)
     q = np.zeros([n, n])
+#    seed = 514
+#    rng = np.random.default_rng(514)
+#    q = 2.0 * rng.random([n, n]) - 1.0
     psi = np.zeros([n, n])
-    dqdt = step(q, psi, y)
-    params = psi, y
     for i in range(nstep+1): 
-        print(f"step {i} qmin={q.min()} qmax={q.max()}")
-        q[1:-1, 1:-1] += rk4(step, q, dt, *params)[1:-1, 1:-1]
+        print(f"step {i} p: min={psi.min():5.2e} max={psi.max():5.2e} q: min={q.min():5.2e} max={q.max():5.2e}")
+#        q[1:-1, 1:-1] += rk4(step, q, dt, psi, y, c=0.0, f=0.0, eps=1.0, tau0=0.0)[1:-1, 1:-1]
+        q[1:-1, 1:-1] += rk4(step, q, dt, psi, y)[1:-1, 1:-1]
         if i % nsave == 0:
             np.save(f"q{i:05d}.npy", q)
+            np.save(f"psi{i:05d}.npy", psi)
 
     