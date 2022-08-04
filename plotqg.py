fce218745192e2fabf3d9a47a9034b6c9c496084
eno <>
2022-08-04 05:47:12 +0000

    check save before proceed
    FossilOrigin-Name: a370e7e874e123e39883487baf0cf8d6f9da131f2133ab623a2b740d0eef3a1a

--- plotqg.py
+++ plotqg.py
@@ -2,20 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-t = 1000
+t = 400
 q = np.load(f"q{t:05d}.npy").T
-psi = np.load(f"psi{t:05d}.npy").T
+psi = np.load(f"p{t:05d}.npy").T
 n = q.shape[0]
 x = np.linspace(0, 1, n)
 y = np.linspace(0, 1, n)
 plt.rcParams["font.size"] = 18
 fig, axs = plt.subplots(1, 2, figsize=[14, 6])
 z = [q, psi]
-labels = ["pv", r"$\psi$"]
+zmax = [0.6, 1.2e-3]
+title = ["pv", r"$\psi$"]
 for i in range(len(axs)):
     ax = axs[i]
-    c = ax.contourf(x, y, z[i])
+    c = ax.pcolormesh(x, y, z[i],
+            cmap="RdYlBu_r", vmin=-zmax[i], vmax=zmax[i])
+    ax.set_title(title[i])
     ax.set_aspect("equal")
-    fig.colorbar(c, ax=ax)
+    fig.colorbar(c, ax=ax, shrink=0.8)
+fig.suptitle(r"$t=$"+f"{t}")
 plt.show()
 
