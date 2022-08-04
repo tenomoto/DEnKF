0e624919bf209f2875e5689f92b3e9f88ac0e777
eno <>
2022-08-04 05:55:51 +0000

    resolve conflict
    FossilOrigin-Name: b6fcf67a681ce5d0a18721a7cc55bd54e408a39c66131298517fc399af116160

--- plotqg.py
+++ plotqg.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 
 t = 400
-q = np.load(f"q{t:05d}.npy").T
-psi = np.load(f"p{t:05d}.npy").T
+q = np.load(f"q{t:06d}.npy").T
+psi = np.load(f"p{t:06d}.npy").T
 n = q.shape[0]
 x = np.linspace(0, 1, n)
 y = np.linspace(0, 1, n)
@@ -12,14 +12,16 @@ plt.rcParams["font.size"] = 18
 fig, axs = plt.subplots(1, 2, figsize=[14, 6])
 z = [q, psi]
 zmax = [0.6, 1.2e-3]
-title = ["pv", r"$\psi$"]
+title = [r"stream function $\psi$", r"potential vorticity $q$"]
 for i in range(len(axs)):
     ax = axs[i]
     c = ax.pcolormesh(x, y, z[i],
             cmap="RdYlBu_r", vmin=-zmax[i], vmax=zmax[i])
     ax.set_title(title[i])
     ax.set_aspect("equal")
+#fig.savefig(f"pq{t:06d}.png")
     fig.colorbar(c, ax=ax, shrink=0.8)
 fig.suptitle(r"$t=$"+f"{t}")
+fig.tight_layout()
 plt.show()
 
--- qg.py
+++ qg.py
@@ -5,7 +5,7 @@ from mg import v_cycle
 from ode import rk4
 
 
-def step(q, psi, y, c=1.0, f=1600, eps=1.0e-5, a=2.0e-12, tau0=tau, itermax=(1, 1, 5000), tol=1.0e-4):
+def step(q, psi, y, c, f, eps, a, tau0, itermax, tol):
     d = y[1] - y[0]
     d2 = d * 2
     dd = d ** 2
@@ -21,21 +21,26 @@ def step(q, psi, y, c=1.0, f=1600, eps=1.0e-5, a=2.0e-12, tau0=tau, itermax=(1, 
 if __name__ == "__main__":
     n = 129 
     dt = 1.5
-    nstep = 1000
-    nsave = 100
+    tsave = 100000 
+    nstep = 500000
+    nsave = 200
+    itermax = 1, 1, 100
+    datadir="free"
     x = np.linspace(0.0, 1.0, n)
     y = np.linspace(0.0, 1.0, n)
     q = np.zeros([n, n])
-#    params = ()
-    params = 0.0, 0.0, 1.0, 2.0e-12, 0.0
-    seed = 514
-    rng = np.random.default_rng(514)
-    q[1:-1, 1:-1] = 2.0 * rng.random([n-2, n-2]) - 1.0
+#    seed = 514
+#    rng = np.random.default_rng(514)
+#    q[1:-1, 1:-1] = 2.0 * rng.random([n-2, n-2]) - 1.0
     psi = np.zeros([n, n])
+#    c, f, eps, a, tau0 = 0.0, 0.0, 1.0, 2.0e-12, 0.0
+    c, f, eps, a, tau0 = 1.0, 1600, 1.0e-5, 2.0e-12, -tau
+    itermax = 1, 1, 100
+    tol = 1.0e-4
+    params = psi, y, c, f, eps, a, tau0, itermax, tol
     for i in range(nstep+1): 
         if i % nsave == 0:
-            np.save(f"q{i:05d}.npy", q)
-            np.save(f"p{i:05d}.npy", psi)
+            np.save(f"q{i:06d}.npy", q)
+            np.save(f"p{i:06d}.npy", psi)
         print(f"step {i} p: min={psi.min():5.2e} max={psi.max():5.2e} q: min={q.min():5.2e} max={q.max():5.2e}")
         q[1:-1, 1:-1] += rk4(step, q, dt, psi, y, *params)[1:-1, 1:-1]
-
